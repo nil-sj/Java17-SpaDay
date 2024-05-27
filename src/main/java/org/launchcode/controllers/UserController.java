f8a5d52843748c70e9b8ac04b54a361abeec6cad
Nil SJ <niladri123@gmail.com>
2024-05-27 15:07:07 -0400

    Completed SPA User Validation Studio - sans Bonus

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,16 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
+
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)){
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        if(errors.hasErrors()) {
+            model.addAttribute("errorMessage", "Bad data! Please correct the following errors:");
+            return "user/add";
+        } else if (user.getPassword().equals(verify)){
             return "user/index";
         } else {
             model.addAttribute("error", "Error: The password should match!");
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,10 +1,31 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
+    @NotBlank
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters." )
     private String username;
+
+    @Email(message="Invalid email. Try again.")
     private String email;
+    @NotBlank
+    @Size(min=6, message="The password should be at least 6 characters long.")
     private String password;
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
@@ -29,4 +50,5 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
